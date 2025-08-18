0ab58b315494bb46c3b7a9e9a6f35592558444a7
gabdourado <gab.dourado.if23@gmail.com>
2025-08-18 17:25:46 -0300

    Questão 1006

--- Iniciante/C++/Beecrowd1006.cpp
+++ Iniciante/C++/Beecrowd1006.cpp
@@ -1,9 +1,18 @@
-#include <stdio.h>
+#include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main (void) {
+    double A, B, C, MEDIA;
+    float PA (2.0), PB (3.0), PC (5.0);
     
-
+    cin >> A >> B >> C;
+    
+    MEDIA = (A*PA + B*PB + C*PC) / (PA + PB + PC);
+    
+    cout << fixed << setprecision(1);
+    cout << "MEDIA = " << MEDIA << endl;
+    
     return 0;
 }
