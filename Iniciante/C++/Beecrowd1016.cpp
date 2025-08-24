98d891fda89bab8e3f3cb8cfe6909a037cffc102
gabdourado <gab.dourado.if23@gmail.com>
2025-08-24 18:20:32 -0300

    Questão 1016

---
+++ Iniciante/C++/Beecrowd1016.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void) {
+
+    int dist, time;
+    
+    cin >> dist;
+
+    time = 2*dist;
+
+    cout << time << " minutos" << endl;
+
+    return 0;
+}
